d5102e82ecf784aae77d4e213f8ec8b8413dcee1
Debraj Dalai <debrajdalai29@gmail.com>
2026-04-28 13:19:31 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import userRouter from "./src/routes/user.route.js";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://auth-system-seven-phi.vercel.app", 
   credentials: true
 }));
 app.use(express.json());
