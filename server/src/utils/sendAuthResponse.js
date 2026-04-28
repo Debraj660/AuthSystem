32cdecd92cc14e88c249a4f09c326120ca10c6c7
Debraj Dalai <debrajdalai29@gmail.com>
2026-04-28 13:28:38 +0530

    http configure

--- server/src/utils/sendAuthResponse.js
+++ server/src/utils/sendAuthResponse.js
@@ -3,8 +3,8 @@ import ApiResponse from "./ApiResponse.js";
 export const sendAuthResponse = (res, accessToken, refreshToken, message, statusCode = 200) => {
     const cookiePayload = {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24 * parseInt(process.env.MAX_AGE),
     };
 
